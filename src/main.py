df67929d022681c62494b99084197d25ef2c2b7a
Alina.Noskova <alina.noskova@quantori.com>
2024-05-23 18:03:20 +0300

    change directory_path

--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
+import os
 from database import create_database, insert_xyz_data, view_table, delete_database
 
 db_path = 'xyz_molecules.db'
-directory_path = 'C:/Users/AlinaNoskova/PycharmProjects/Molecular_Proj/data/structures'
+# Absolute path to the directory with the current script (src)
+current_directory = os.path.dirname(os.path.abspath(__file__))
+# Path to the project root directory
+project_directory = os.path.dirname(current_directory)
+# Relative path to the data folder
+directory_path = os.path.join(project_directory, 'data', 'structures')
 
 create_database(db_path)
 insert_xyz_data(db_path, directory_path)
 view_table(db_path)
-#delete_database(db_path)
+# delete_database(db_path)
