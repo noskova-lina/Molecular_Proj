17d31dc0ae30b925601c8837f05c38e31d0403e5
Alina.Noskova <alina.noskova@quantori.com>
2024-07-09 16:13:16 +0300

    Removing unused variables

--- src/settings.py
+++ src/settings.py
@@ -1,11 +1,4 @@
-import os
-
 db_path = 'xyz_molecules.db'
-directory_path = str("../data/structures")
 zip_file_path = str("../data/structures.zip")
 zip_file_scalar_coupling_path = str("../data/scalar_coupling_constants.zip")
 zip_file_scalar_contributions_path = str("../data/scalar_coupling_contributions.zip")
-
-# current_directory = os.path.dirname(os.path.abspath(__file__))
-# project_directory = os.path.dirname(current_directory)
-# directory_path = os.path.join(project_directory, 'data', 'structures')
